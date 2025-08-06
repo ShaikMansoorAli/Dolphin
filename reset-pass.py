9398089b3b1c379af27021f1be24a2d2d8010cf6
ShaikMansoorAli <shaikmansoorali57@gmail.com>
2025-08-06 21:34:18 +0530

    Delete reset-pass.py

--- reset-pass.py
+++
@@ -1,7 +0,0 @@
-username = input("enter username")
-password = input("enter password")
-
-if username == "admin" and password == "admin":
-    print("U can reset")
-else:
-    print("U cannot  reset")
